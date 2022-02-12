5c7c652f2c3c6107519d1d5299c982bb8fabf157
Brawer Nuñez <n.eedar@hotmail.com>
2022-02-12 01:47:52 -0500

    finished Stories component

---
+++ interfaces/Stories.d.ts
@@ -0,0 +1,25 @@
+export interface Suggestion {
+  id: number;
+  name: string;
+  username: string;
+  avatar: string;
+  email: string;
+  dob: Date;
+  phone: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
